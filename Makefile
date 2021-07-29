d9efd5281ed8d2f5a597564c96801b1315297780
Benjamin A. Himes <himes.benjamin@gmail.com>
2021-07-29 08:59:08 -0400

    Added fftw3f to be able to test cpu comps

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 NVCC=nvcc
 
-NVCC_FLAGS=-ccbin=g++ -t 4
-NVCC_FLAGS+=--gpu-architecture=sm_70 -gencode=arch=compute_70,code=compute_70
+NVCC_FLAGS=-ccbin=icpc -t 4
+NVCC_FLAGS+=--gpu-architecture=sm_70 -gencode=arch=compute_70,code=compute_70 
 #NVCC_FLAGS+=--gpu-architecture=sm_86 -gencode=arch=compute_86,code=compute_86 --generate-code arch=compute_70,code=sm_70 --generate-code arch=compute_75,code=sm_75 --generate-code arch=compute_80,code=sm_80
-NVCC_FLAGS+=--default-stream per-thread -m64 -O3 --use_fast_math --extra-device-vectorization -std=c++17  -Xptxas --warn-on-local-memory-usage,--warn-on-spills, --generate-line-info 
+NVCC_FLAGS+=--default-stream per-thread -m64 -O3 --use_fast_math --extra-device-vectorization -std=c++17  -Xptxas --warn-on-local-memory-usage,--warn-on-spills, --generate-line-info -Xcompiler=-std=c++17
 
 CUFFTDX_INCLUDE_DIR=$(FastFFT_cufftdx_dir)/include
 
@@ -20,10 +20,10 @@ SRCS=test.cu
 TARGETS=test
 
 $(TARGETS): %: %.cu
-	$(NVCC) -o $@.app $< $(NVCC_FLAGS) -I$(CUFFTDX_INCLUDE_DIR)
+	$(NVCC) -o $@.app $< $(NVCC_FLAGS) -I$(CUFFTDX_INCLUDE_DIR) -lfftw3f
 
 $(NVRTC_TARGETS): %: %.cu
-	$(NVCC) -o $@ $< $(NVCC_FLAGS) -I$(CUFFTDX_INCLUDE_DIR) $(NVRTC_DEFINES) -lnvrtc -lcuda
+	$(NVCC) -o $@ $< $(NVCC_FLAGS) -I$(CUFFTDX_INCLUDE_DIR) $(NVRTC_DEFINES) -lnvrtc -lcuda 
 
 .PHONY: all clean
 
--- test.cu
+++ test.cu
@@ -3,27 +3,54 @@
 #include <chrono>
 #include <cmath>
 
+// sudo apt-get install libfftw3-dev libfftw3-doc
+#include <fftw3.h>
+
 #include <cuda_runtime_api.h>
 #include <cufftdx.hpp>
 
 #include "FastFFT.cu"
 
+
 int main(int, char**) {
   std::printf("Entering main in tests.cpp\n");
 
-  
-  float one[66];
-  for (int i = 0; i < 66; i++) { one[i] = 1.f ;}
-  for (int i = 0; i < 10; i++) { std::printf("Before, i[%d] = %f\n", i, one[i]);}
+  std::printf("Standard is %i\n",__cplusplus);
+  float* real_values;
+  float2* complex_values;
+  int real_memory_allocated;
+  int wanted_x_size = 64;
+  int wanted_y_size = 1;
+  int wanted_z_size = 1;
+
+  	// first_x_dimension
+	if (wanted_x_size % 2 == true) real_memory_allocated =  wanted_x_size / 2 + 1;
+	else real_memory_allocated = (wanted_x_size - 1) / 2 + 1;
+
+	real_memory_allocated *= wanted_y_size * wanted_z_size; // other dimensions
+	real_memory_allocated *= 2; // room for complex
+
+//  real_values = new float[real_memory_allocated];
+	real_values = (float *) fftwf_malloc(sizeof(float) * real_memory_allocated);
+	complex_values = (float2*) real_values;  // Set the complex_values to point at the newly allocated real values;
+
+  fftwf_plan plan_fwd = NULL;
+	plan_fwd = fftwf_plan_dft_r2c_3d(1, 1, 64, real_values, reinterpret_cast<fftwf_complex*>(complex_values), FFTW_ESTIMATE);
+	//plan_bwd = fftwf_plan_dft_c2r_3d(logical_z_dimension, logical_y_dimension, logical_x_dimension, reinterpret_cast<fftwf_complex*>(complex_values), real_values, FFTW_ESTIMATE);
+
+  for (int i = 0; i < 66; i++) { real_values[i] = 1.f ;}
+  for (int i = 0; i < 10; i++) { std::printf("Before, i[%d] = %f\n", i, real_values[i]);}
   FastFFT::FourierTransformer FT(FastFFT::FourierTransformer::DataType::fp32);
 //  FT.SetInputDimensionsAndType(64,1,1,true, false,(wanted_data_type, wanted_origin);    
 	FT.SetInputDimensionsAndType(64,1,1,true, false,FastFFT::FourierTransformer::DataType::fp32, FastFFT::FourierTransformer::OriginType::natural);
 	FT.SetOutputDimensionsAndType(64,1,1,true,FastFFT::FourierTransformer::DataType::fp32, FastFFT::FourierTransformer::OriginType::natural);
-	FT.SetInputPointer(&one[0], false);
+	FT.SetInputPointer(&real_values[0], false);
 	FT.CopyHostToDevice();
-  for (int i = 0; i < 66; i++) { one[i] = 0.f ;}
-  for (int i = 0; i < 10; i++) { std::printf("After zero, i[%d] = %f\n", i, one[i]);}
+  for (int i = 0; i < 66; i++) { real_values[i] = 0.f ;}
+  for (int i = 0; i < 10; i++) { std::printf("After zero, i[%d] = %f\n", i, real_values[i]);}
 	FT.CopyDeviceToHost(true, true);
-  for (int i = 0; i < 10; i++) { std::printf("After copy back, i[%d] = %f\n", i, one[i]);}
+  for (int i = 0; i < 10; i++) { std::printf("After copy back, i[%d] = %f\n", i, real_values[i]);}
+
+  delete [] real_values;
   
 }
